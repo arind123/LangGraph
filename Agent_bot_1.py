6add247346e72a027031196f619b8b9be0c44a76
Arindam Adak <36856593+arind123@users.noreply.github.com>
2026-01-18 01:08:21 +0530

    commit_1

---
+++ Agent_bot_1.py
@@ -0,0 +1,93 @@
+
+##################################
+####### BOT WITHOUT MEMORY #########
+##################################
+
+# from langgraph.graph import StateGraph, END
+# from langchain_ollama import ChatOllama
+# from langchain_core.messages import HumanMessage, AIMessage
+# from langgraph.graph import StateGraph, START, END
+# from pydantic import BaseModel, Field
+
+
+# class AgentState(BaseModel):
+#     messages: list[HumanMessage]
+
+# llm = ChatOllama(
+#     model="llama3", # or "mistral", "gemma", etc.
+#     temperature=0,
+# )
+
+# def process(state: AgentState) -> AgentState:
+#     response = llm.invoke(state.messages)
+#     print(f"\nAI: {response.content}")
+#     return state
+
+# graph = StateGraph(AgentState)
+# graph.add_node("process", process)
+# graph.add_edge(START, "process")
+# graph.add_edge("process", END) 
+# agent = graph.compile()
+
+# user_input = input("Enter: ")
+# while user_input != "exit":
+#     agent.invoke({"messages": [HumanMessage(content=user_input)]})
+#     user_input = input("Enter: ")
+
+##################################
+####### BOT WITH MEMORY #########
+##################################
+
+
+from langgraph.graph import StateGraph, END, START
+from langchain_ollama import ChatOllama
+from langchain_core.messages import HumanMessage, AIMessage
+from langgraph.graph import StateGraph, START, END
+from typing import List, Union
+from pydantic import BaseModel, Field
+
+class AgentState(BaseModel):
+    messages: List[Union[HumanMessage, AIMessage]]
+
+llm = ChatOllama(
+    model="llama3", # or "mistral", "gemma", etc.
+    temperature=0,
+)
+
+def process(state: AgentState) -> AgentState:
+    """This node will solve the request you input"""
+    response = llm.invoke(state.messages)
+
+    state.messages.append(AIMessage(content=response.content)) 
+    print(f"\nAI: {response.content}")
+    print("CURRENT STATE: ", state.messages)
+
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END) 
+agent = graph.compile()
+
+
+conversation_history = []
+
+user_input = input("Enter: ")
+while user_input != "exit":
+    conversation_history.append(HumanMessage(content=user_input))
+    result = agent.invoke({"messages": conversation_history})
+    conversation_history = result["messages"]
+    user_input = input("Enter: ")
+
+with open("logging.txt", "w") as file:
+    file.write("Your Conversation Log:\n")
+    
+    for message in conversation_history:
+        if isinstance(message, HumanMessage):
+            file.write(f"You: {message.content}\n")
+        elif isinstance(message, AIMessage):
+            file.write(f"AI: {message.content}\n\n")
+    file.write("End of Conversation")
+
+print("Conversation saved to logging.txt")
---
+++ ReAct.py
@@ -0,0 +1,102 @@
+# ReAct Agent,  Reasoning and Acting Agent
+# Create Tools in Langgraph
+# Create a ReAct Graph
+# Different Types of Messeges such as ToolMessages, SystemMessages
+
+from typing import Annotated, Sequence, TypedDict
+import os
+from dotenv import load_dotenv
+
+from dotenv import load_dotenv  
+from langchain_core.messages import BaseMessage # The foundational class for all message types in LangGraph
+from langchain_core.messages import ToolMessage # Passes data back to LLM after it calls a tool such as the content and the tool_call_id
+from langchain_core.messages import SystemMessage # Message for providing instructions to the LLM
+# from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langgraph.graph.message import add_messages #Reducer function
+from langgraph.graph import StateGraph, END
+from langgraph.prebuilt import ToolNode
+from pydantic import BaseModel, Field
+import math
+
+
+load_dotenv()
+
+class AgentState(BaseModel):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+
+@tool
+def add(numbers: list[float|int]) -> float|int:
+    '''This is a tool to perform addition on a list of numbers.'''
+    return sum(numbers)
+
+@tool
+def multiply(numbers: list[float|int]) -> float|int:
+    '''This is a tool to multiply a list of numbers.'''
+    result = 1
+    for num in numbers:
+        result *= num
+    return result
+
+@tool
+def subtract(a: float|int , b: float|int) -> float|int:
+    '''Function to subtract two inputs.'''
+    return a-b
+
+@tool
+def division(a: float|int, b: float|int) -> float|int:
+    '''Function to perform divison'''
+    return a/b
+
+tools = [add, multiply, subtract, division]
+# model = ChatOllama(model="mistral").bind_tools(tools)
+model = ChatOpenAI(model = "gpt-4o").bind_tools(tools)
+
+
+
+def model_call(state: AgentState) -> AgentState:
+    '''Call the llm model with tools and system prompt.'''
+    system_prompt = SystemMessage(content = "You are my AI assistant, please answer my query to the best of your ability.")                          
+    response = model.invoke([system_prompt] + state.messages)
+    return {'messages': [response]}
+
+def should_continue(state: AgentState):
+    messages = state.messages
+    last_message = messages[-1]
+    if not last_message.tool_calls:
+        return "end"
+    else:
+        return "continue"
+
+graph = StateGraph(AgentState)
+graph.add_node("our_agent", model_call)
+
+tool_node = ToolNode(tools = tools)
+graph.add_node("tools", tool_node)
+
+graph.set_entry_point("our_agent")
+graph.add_conditional_edges(
+    "our_agent",
+    should_continue,
+    {
+        "continue" : "tools",
+        "end": END
+    }
+)
+
+graph.add_edge("tools", "our_agent")
+
+app = graph.compile()
+
+def print_stream(stream):
+    for s in stream:
+        message = s["messages"][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+
+inputs = {"messages": [("user", "What will be the result if we add 1,9,8 and 7 then multiply the result by 7.")]}
+print_stream(app.stream(inputs, stream_mode="values"))
